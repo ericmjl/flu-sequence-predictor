2fa6bcc577e63aed8dd098ead13e3418cb73a617
Eric Ma <ericmajinglong@gmail.com>
2017-06-23 12:49:58 -0400

    PEP8 fixes

--- utils/data.py
+++ utils/data.py
@@ -3,7 +3,6 @@ import pandas as pd
 import logging
 from datetime import datetime
 
-
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
@@ -14,8 +13,9 @@ def load_sequence_and_metadata(kind='feather'):
     DataFrame.
     """
     starttime = datetime.now()
-    sequences = [s for s in SeqIO.parse('data/20170531-H3N2-global.fasta',
-                                        'fasta')]
+    sequences = [s for s
+                 in SeqIO.parse('data/20170531-H3N2-global.fasta', 'fasta')
+                 ]
     if kind == 'csv':
         metadata = pd.read_csv('data/20170531-H3N2-global.tsv',
                                sep='\t',
@@ -34,6 +34,7 @@ def load_prediction_coordinates():
     as a pandas DataFrame.
     """
     logger.debug('started load_prediction_coordinates()')
-    df = pd.read_csv('data/oneQ_prediction_coords_with_colors.csv', index_col=0)
+    df = pd.read_csv('data/oneQ_prediction_coords_with_colors.csv',
+                     index_col=0)
     logger.debug('finished load_prediction_coordinates()')
     return df
