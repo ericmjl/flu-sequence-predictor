d07eaceeb3af8af17438687fb6c7f30b8a4d7e19
Eric Ma <ericmajinglong@gmail.com>
2019-05-31 18:06:33 -0400

    switched to reading csv

--- utils/data.py
+++ utils/data.py
@@ -8,24 +8,26 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-def load_sequence_and_metadata(kind='feather'):
+def load_sequence_and_metadata(kind="csv"):
     """
     Returns the sequences as a list of SeqRecords, and metadata as a pandas
     DataFrame.
     """
     starttime = datetime.now()
-    sequences = [s for s
-                 in SeqIO.parse('data/20170531-H3N2-global.fasta', 'fasta')
-                 ]
-    if kind == 'csv':
-        metadata = pd.read_csv('data/20170531-H3N2-global.tsv',
-                               sep='\t',
-                               parse_dates=['Collection Date'])
-    elif kind == 'feather':
-        metadata = pd.read_feather('data/20170531-H3N2-global.feather')
+    sequences = [
+        s for s in SeqIO.parse("data/20170531-H3N2-global.fasta", "fasta")
+    ]
+    if kind == "csv":
+        metadata = pd.read_csv(
+            "data/20170531-H3N2-global.tsv",
+            sep="\t",
+            parse_dates=["Collection Date"],
+        )
+    elif kind == "feather":
+        metadata = pd.read_feather("data/20170531-H3N2-global.feather")
     endtime = datetime.now()
     elapsed = endtime - starttime
-    print(f'load_sequence_and_metadata() took {elapsed} seconds.')
+    print(f"load_sequence_and_metadata() took {elapsed} seconds.")
     return sequences, metadata
 
 
@@ -34,8 +36,9 @@ def load_prediction_coordinates():
     Returns the coordinates of the predictions, and their associated colours,
     as a pandas DataFrame.
     """
-    logger.debug('started load_prediction_coordinates()')
-    df = pd.read_csv('data/oneQ_prediction_coords_with_colors.csv',
-                     index_col=0)
-    logger.debug('finished load_prediction_coordinates()')
+    logger.debug("started load_prediction_coordinates()")
+    df = pd.read_csv(
+        "data/oneQ_prediction_coords_with_colors.csv", index_col=0
+    )
+    logger.debug("finished load_prediction_coordinates()")
     return df
